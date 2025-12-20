d6415815f5050fefcaef69d69c325df2b5c5fa13
vadim-severov <vold.severov@mail.ru>
2025-12-20 12:11:56 +0000

    Лабораторная работа 7

--- sites/skuit/www/index.php
+++ sites/skuit/www/index.php
@@ -1,3 +1,3 @@
 <?
-include_once './lab0/index.php';
+include_once './lab7/index.php';
 ?>
---
+++ sites/skuit/www/lab7/index.php
@@ -0,0 +1,36 @@
+<?php
+// Задача 3
+$sum = 1;
+$n = rand(10,100);
+for ($i=1; $i<$n; $i++) {
+    $sum = $sum + $i; 
+}
+
+echo $n;
+echo '<br>';
+echo $sum;
+
+// Задача 4
+$list = [];
+for ($i=0; $i<10; $i++) {
+    $list []= rand(-10,10);
+}
+$positive = 0;
+$zero = 0;
+$negative = 0;
+foreach ($list as $value) {
+    echo $value . ' ';
+    if ($value > 0) {
+        $positive = $positive + 1;
+    } elseif ($value < 0) {
+        $negative = $negative + 1;
+    } else {
+        $zero++;
+    }
+}
+echo '<br>';
+echo 'Количество положительных чисел = ' . $positive;
+echo '<br>';
+echo 'Количество отрицательных чисел = ' . $negative;
+echo '<br>';
+echo 'Количество нулей = ' . $zero;
